d691947006136f5d8f4cc91fc8fede4fac42065a
Matt Czencz <mattczencz24@gmail.com>
2021-11-23 17:39:43 -0700

    Fixed show page. Added ShowHero for show page heros and styling to go along with it.

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,27 +1,33 @@
+import "../styles/show.scss"
+import ShowHero from '../components/ShowHero'
+
 function Show(props) {
-    // Breakdown props
     const id = props.match.params.id
-    
-    // const allMakeup = props.allMakeup
-    // const item = allMakeup.find(i => i.id === id)
-    console.log(id)
+    const allMakeup = props.allMakeup
+    const item = allMakeup.find(i => i.id.toString() === id)
+    console.log(item)
 
     // Function for data to be loaded
     const loaded = () => {
         return(
-        <div>
-            <h1>Hello</h1>
-        </div>
+            <>
+            <ShowHero text={item.name} img={item.image_link} rating={item.rating} price={item.price}/>
+            <div className="content">
+                <p>{item.description}</p>
+                <a href={item.product_link} target="_blank">Purchase This Product</a>
+            </div>
+        </>
         )
     }
-        
+    
+    // Function for while data is loading
     const loading = () => (
         <h1>Loading...</h1>
     )
 
     return (
         <section>
-            {props.allMakeup ? loaded() : loading()}
+            {allMakeup ? loaded() : loading()}
         </section>
     )
 }
