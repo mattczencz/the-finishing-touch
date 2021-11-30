92bc2119549c715ef39fee52fd5841741893c877
ronald <ronaldgutierrezj@gmail.com>
2021-11-30 17:28:06 -0600

    button fixed

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -8,27 +8,56 @@ function Show(props) {
     const item = allMakeup.find(i => i.id.toString() === id)
     console.log(item)
 
+    const [newForm, setNewForm] = useState({
+        brand: "",
+        name: "",
+        quantity: 0,
+        price: 0,
+        imagen_link: "",
+        description: "",
+        product_type: "",
+        tag_list: "",
+    })
+    const handleChange =()=>{
+       
+        setNewForm({ brand : item.brand,
+            name: item.name,
+            quantity: newForm.quantity + 1,
+            price: item.price,
+            imagen_link: item.image_link,
+            description: item.description,
+            product_type: item.product_type,
+            tag_list:item.tag_list
+            });
+        addItem(newForm)
+        
+    }
+
+
+    
+
     const URL = "https://the-finishing-touch.herokuapp.com/cart/";
 
-    const addItem = async (item) => {
+    const addItem = async (newForm) => {
         await fetch(URL, {
             method: 'post',
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify(item),
+            body: JSON.stringify(newForm),
         })
     }
 
     // Function for data to be loaded
     const loaded = () => {
+       
         return(
             <>
             <ShowHero text={item.name} img={item.image_link} rating={item.rating} price={item.price}/>
             <div className="content">
                 <p>{item.description}</p>
                 <a href={item.product_link} target="_blank">Purchase This Product</a>
-                <button onSubmit={addItem(item)}>Add To Cart</button>
+                <button onclick={handleChange}>Add To Cart</button>
             </div>
         </>
         )
